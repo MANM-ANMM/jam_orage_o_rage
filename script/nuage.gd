46defa3237e3f0703f4b2af982662b8ce92b717c
Martin Rouault <mrouault@hotmail.fr>
2024-03-09 11:51:57 +0100

    base nuages

---
+++ script/nuage.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+@onready var timer_pluie := $TimerPluie
+var pluie_on := false
+
+var speed := PI/randf_range(80, 205)
+var sens :float= [1].pick_random()
+
+func _ready():
+	$altitude.position.y *= (1+speed+randf_range(-0.01, 0.01))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate(delta*speed*sens)
+
+func commencer_pluie():
+	timer_pluie.start()
+	pluie_on = true
+	speed /= 2
+
+func _on_timer_debut_pluie_timeout():
+	if not pluie_on:
+		commencer_pluie()
+
+
+func _on_timer_pluie_timeout():
+	queue_free()
