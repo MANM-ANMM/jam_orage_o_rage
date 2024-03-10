84aaf0d0373ccf2364018f447d07075716ed39ea
Martin Rouault <mrouault@hotmail.fr>
2024-03-10 15:08:26 +0100

    canon

---
+++ script/constructions/boulet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name Boulet
+
+var vitesse : Vector2
+@onready var planete : Node2D = get_parent()
+@export var peuple : Types.Peuple
+
+func _physics_process(delta):
+	vitesse += (planete.global_position-global_position).normalized()*900.0*delta
+	position += vitesse*delta
+
+
+func _on_body_entered(body):
+	if body.has_method("subir_degats") and body.peuple == peuple:
+		body.subir_degats(1)
+	queue_free()
+	
---
+++ script/constructions/lanceur_boulet.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+@export var scene_boulet : PackedScene
+
+@export var peuple : Types.Peuple
+@onready var timer := $Timer
+
+var compteur := 0:
+	set(val):
+		compteur = val
+		timer.paused = compteur==0
+
+
+func _on_body_entered(body):
+	if body.peuple != peuple:
+		compteur+=1
+
+
+func _on_body_exited(body):
+	if body.peuple != peuple:
+		compteur-=1
+
+func tirer():
+	var b :Boulet= scene_boulet.instantiate()
+	b.global_position = global_position
+	var cible :Node2D= get_cibles().front()
+	print(cible)
+	var distance := cible.global_position.distance_to(global_position)
+	var puissance = 0.8855*distance+192.19
+	# 305 461
+	# 460 600
+	# 224.383636474609 391.753890962646
+	print(distance," ", puissance)
+	b.vitesse = Vector2.from_angle(rotation)*puissance
+	Nodes.node_personnages.add_child(b)
+
+func _on_timer_timeout():
+	tirer()
+
+func get_cibles():
+	var cibles := get_overlapping_bodies().filter(func(b): return b.peuple != peuple)
+	cibles.sort_custom(compare_cibles)
+	return cibles
+
+func compare_cibles(a, b):
+	var da = a.global_position.distance_squared_to(global_position)
+	var db = b.global_position.distance_squared_to(global_position)
+	return da<db
